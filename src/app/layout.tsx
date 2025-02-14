250a523507ca8317395008d799e22a39a468bb34
Izaias Lima <izaiaslima356@gmail.com>
2025-02-13 21:29:38 -0300

    build: set up next shadcn ui app

---
+++ src/app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Geist } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+	variable: "--font-geist-sans",
+	subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+	title: "Ignite Call",
+	description: "Agendamento Descomplicado",
+};
+
+export default function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return (
+		<html lang="pt-br">
+			<head>
+				<link rel="icon" href="/favicon.ico" sizes="any" />
+			</head>
+
+			<body className={`${geistSans.className} antialiased`}>{children}</body>
+		</html>
+	);
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,40 @@
+import Image from "next/image";
+
+import appPreviewImage from "../assets/app-preview.png";
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+	return (
+		<div className="bg-zinc-950 text-white">
+			<div
+				className="flex p-0 max-w-[calc(100vw-((100vw-1160px)/2))] ml-auto h-screen
+			gap-20 items-center"
+			>
+				<div className="max-w-[500px] py-0 px-10 flex flex-col gap-4">
+					<h1 className="text-6xl font-bold leading-tight">
+						Agendamento descomplicado
+					</h1>
+
+					<p className="text-lg text-muted-foreground">
+						Conecte seu calendário e permita que as pessoas marquem agendamentos
+						no seu tempo livre.
+					</p>
+
+					<Button className="bg-red-600 hover:bg-red-700 max-w-max">
+						Criar conta com Google
+					</Button>
+				</div>
+
+				<div className="hidden md:block">
+					<Image
+						src={appPreviewImage}
+						alt="Calendário simbolizando a aplicação em funcionamento"
+						height={400}
+						quality={100}
+						priority
+					/>
+				</div>
+			</div>
+		</div>
+	);
+}
